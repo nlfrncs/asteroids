1d1c8c20b9229fbdde723582f71950c19b98d031
noelnoel <noelfranciscarreon@gmail.com>
2025-05-04 21:51:49 +0800

    add player files

--- main.py
+++ main.py
@@ -1,7 +1,39 @@
 import pygame
+from constants import *
+from circleshape import *
+from player import *
 
 def main():
     print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    clock = pygame.time.Clock()
+    dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
+
+    while(1):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        
+        screen.fill("black")
+        for item in drawable:
+            item.draw(screen)
+        updatable.update(dt)
+        pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
